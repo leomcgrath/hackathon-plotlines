6aee1a06e64361b5ed03e4ec910f637f44e6c3c8
Magnus Ramm <magnus.ramm@gmail.com>
2025-04-22 18:20:05 +0200

    Veldig veldig

--- backend/api/tips.ts
+++ backend/api/tips.ts
@@ -3,8 +3,8 @@ import express, {type Request, type Response } from "express";
 const tipsRoute = express.Router();
 
 const tips: string[] = [
-  "Når man legger til en ny route, så må man huske å legge de til i listen i <code>routes.ts</code>-filen.",
-  "Sanne er personen man bør gå til om man har vanskelige spørsmål.",
+  "Når man legger til en ny route, så må man huske å legge de til i listen i routes.ts-filen.",
+  "Sanne er personen man bør gå til om man har veldig veldig vanskelige spørsmål.",
   "Det er ofte snacks ved vinduet i kantina!",
   "Vi låser Skuret på kvelden, så du kan fint legge igjen sekk og pc."
 ];
